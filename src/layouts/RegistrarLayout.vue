ea398fa703715e589bd49704a544548e57607eb5
Naeem <mangcol.nm57@s.msumain.edu.ph>
2021-10-19 01:23:43 +0800

    Adding a registrar module

---
+++ src/layouts/RegistrarLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="hHh lpR lFf">
+      <q-header elevated class="q-px-lg q-py-md">
+        <q-toolbar>
+          <q-img src="~assets/logo.png" style="width: 110px"></q-img>
+          <q-space />
+          <q-card flat class="bg-transparent">
+            <div class="font2 text-h3 flex flex-center">Registrar Module</div>
+            <div class="font2 text-h6 flex flex-center">
+              ______________________________________________________________________________________________
+            </div>
+          </q-card>
+
+          <q-space />
+
+          <q-img src="~assets/msulogo.png" style="width: 110px"></q-img>
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mylayout',
+
+  data() {
+    return {
+      drawer: false,
+      miniState: true,
+    };
+  },
+};
+</script>
