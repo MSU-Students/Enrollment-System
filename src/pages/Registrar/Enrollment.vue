9e42f558dea8be18e05e75c6c8bfbd8bb85bee4f
Naeem <mangcol.nm57@s.msumain.edu.ph>
2022-03-20 22:40:12 +0800

    registrar UI

--- src/pages/Registrar/Enrollment.vue
+++ src/pages/Registrar/Enrollment.vue
@@ -71,7 +71,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Options } from 'vue-class-component';
+import { Vue } from 'vue-class-component';
 
 export default class Enrollment extends Vue {
   text = '';
